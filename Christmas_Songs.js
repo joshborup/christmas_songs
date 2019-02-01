a7265f7aa962a2b1f17986813ff051767c12313e
Josh Borup <joshborup@Joshs-MacBook-Pro.local>
2019-02-01 10:26:15 -0700

    initial

---
+++ Christmas_Songs.js
@@ -0,0 +1,43 @@
+// The group captain will make a NEW repo and add this file to it before following any other instructions
+// the captain will add the other team member/s on the repo as a collaborator
+
+// if you are not the capatian, clone the repo to your machine and follow the instructions below
+
+// you are going to fill out the songs of christmas in the object below as a group
+
+// if you are in a group of 3 fill out only 4 songs per person and then add and commit
+// if you are in a group of 2 fill out 6 songs per person and then save and commit
+
+// you can choose either the real song lyrics or fake ones, but only fill out 4
+
+// before pushing your code, wait for instructions
+
+const daysOfChristmas = {
+  day1: "",
+  day2: "",
+  day3: "",
+  day4: "",
+  day5: "",
+  day6: "",
+  day7: "",
+  day8: "",
+  day9: "",
+  day10: "",
+  day11: "",
+  day12: ""
+};
+
+// Real song lyrics
+
+// A partridge in a pear tree,
+// Two turtle doves,
+// Three french hens,
+// Four calling birds,
+// Five gold rings,
+// Six geese a-laying
+// Seven swans a-swimming,
+// Eight maids a-milking,
+// Nine ladies dancing,
+// Ten lords a-leaping,
+// Eleven pipers piping,
+// Twelve drummers drumming.
---
+++ Groups.md
@@ -0,0 +1,22 @@
+# Eventbrite
+
+    - Sam
+    - Connor
+    - Desiree
+
+# Road Trip App
+
+    - Logan
+    - Brad
+
+# Task Tiger
+
+    - Tiffany
+    - JT
+    - Natalie
+
+# Bands in Town
+
+    - Chris
+    - Kyle
+    - T
